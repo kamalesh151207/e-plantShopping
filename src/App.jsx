ec0844f788bee11708b4e9b66e64d013422bd922
KAMALESH V <kamalesh151207@gmail.com>
2026-05-27 15:14:03 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>🌿 Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery - Your favorite online plant shop.
+      </p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
