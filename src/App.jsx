a284d98b7a48da3380690db551bb016470a5510e
KAMALESH V <kamalesh151207@gmail.com>
2026-05-27 15:25:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ProductList from "./ProductList";
 
 function App() {
+
+  const [showProductList, setShowProductList] = useState(false);
+
   return (
-    <div className="container">
-      <h1>🌿 Paradise Nursery</h1>
+    <div>
 
-      <p>
-        Welcome to Paradise Nursery - Your favorite online plant shop.
-      </p>
+      {!showProductList ? (
 
-      <button>Get Started</button>
+        <div className="container">
+
+          <h1>🌿 Paradise Nursery</h1>
+
+          <p>
+            Welcome to Paradise Nursery - Your favorite online plant shop.
+          </p>
+
+          <button
+            onClick={() => setShowProductList(true)}
+          >
+            Get Started
+          </button>
+
+        </div>
+
+      ) : (
+
+        <ProductList />
+
+      )}
+
     </div>
   );
 }
